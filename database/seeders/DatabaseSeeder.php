2295664e9727187a26d9a8420e5557633021e523
Wilder Canaviri Astete <wildersistemas20@gmail.com>
2021-06-22 10:33:30 -0400

    preparando para bajar cambios de articulos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
          $this->call([
             RoleSeeder::class,
             PermissionSeeder::class,
-            RolePermissionSeeder::class
+            RolePermissionSeeder::class,
             CategorySeeder::class
         ]);
 
