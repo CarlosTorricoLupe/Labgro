427e14d0cdc6fc8fbe76bbe709d6abe47f8bd1df
CarlosTL67 <carlostorricolupe68@gmail.com>
2021-06-29 21:29:57 -0400

    testing error in messages

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -65,8 +65,14 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateCategoryRequest $request, Category $category)
+    public function update(CreateCategoryRequest $request, Category $category)
     {
+        /* $category = Category::findorfail($id);
+        $category->update($request->all());
+        return response()->json([
+            'success'=>true,
+            'message'=>'Categoria eliminada correctamente'
+        ],200); */
         $category->update($request->all());
         return response()->json([
             'sucess' => true,
