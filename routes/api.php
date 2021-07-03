30f5fd120993b8317687501c8705f6952c37cf2c
erickjhunior <erickjuniorquispe20@gmail.com>
2021-07-03 18:52:01 -0400

    "falla seeder"

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UnitController;
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +45,7 @@ Route::apiResource('articles',ArticleController::class);
 Route::get('search', [ArticleController::class, 'searchArticle']);
 Route::get('search_category', [ArticleController::class, 'searchArticleForCategorgy']);
 
-
+Route::apiResource('unit_measure',UnitController::class);
 
 Route::get('/permission', function () {
    /* $user = App\Models\User::where('name','canaviri')->first();
