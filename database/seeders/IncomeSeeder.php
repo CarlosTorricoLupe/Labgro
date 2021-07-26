a4ffc957b4faa7d33f0cec947697a03655f0928b
CarlosTL67 <carlostorricolupe68@gmail.com>
2021-07-26 12:10:09 -0400

    Seeder Incomes

--- database/seeders/IncomeSeeder.php
+++ database/seeders/IncomeSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Income;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class IncomeSeeder extends Seeder
 {
@@ -11,8 +14,26 @@ class IncomeSeeder extends Seeder
      *
      * @return void
      */
+
+     //php artisan db:seed --class=IncomeSeeder 
+     //Por si quieren ejecutar solo el seeder de Incomes
     public function run()
-    {
-        //
+    {      
+        $dates=['2021-08-01','2021-07-12','2021-07-23','2021-07-04','2021-08-22','2021-08-12'];
+
+        for ($i=0; $i < 15; $i++) { 
+            $incom=Income::create([
+                'receipt' => rand(300,1000),
+                'total' => rand(300000,900000)/100,
+                'provider' => 'pedro',
+                'order_number' => rand(100,700),
+                'created_at' => Arr::random($dates)]);
+
+                $articles=Article::inRandomOrder()->take(rand(1,3))->pluck('id');
+                foreach($articles as $article){
+                    $incom->articles()->attach($article,['quantity'=>rand(1,100),'unit_price'=>rand(1000,6000)/100,'total_price'=>rand(10000,20000)/100]);
+                }
+        }
     }
+    
 }
