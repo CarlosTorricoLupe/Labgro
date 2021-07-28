029cbac11375764bd738dd1bc608dbefefdbde64
CarlosTL67 <carlostorricolupe68@gmail.com>
2021-07-27 23:37:48 -0400

    Fix delete category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -95,19 +95,14 @@ class CategoryController extends Controller
                 'articles'=>$articles
             ],200);
         } else {
-            if($category['Active']==true){
-                $category->update(['Active'=>0]);
-                $message="Categoria desactivada correctamente";
-            }else{
-                $category->update(['Active'=>1]);
-                $message="Categoria activada correctamente";
-            }
+            Category::destroy($id);
             return response()->json([
                 'success'=>true,
-                'message'=>$message,
+                'message'=>'Categoria eliminada correctamente',
             ],200);
+            }          
         }
-    }
+    
 
     public function search($name)
     {
