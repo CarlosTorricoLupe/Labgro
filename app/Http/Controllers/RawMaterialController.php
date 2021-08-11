53d5baf7b043a8a2cd6dca64d5136c9b22841498
Wilder Canaviri Astete <wildersistemas20@gmail.com>
2021-08-11 15:22:55 -0400

    Register Raw Material

---
+++ app/Http/Controllers/RawMaterialController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateRawMaterialRequest;
+use App\Models\RawMaterial;
+use Illuminate\Http\Request;
+
+class RawMaterialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateRawMaterialRequest $request)
+    {
+        RawMaterial::create($request->all());
+        return response()->json([
+            'sucess' =>true,
+            'message' =>'Materia prima creada correctamente'
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
