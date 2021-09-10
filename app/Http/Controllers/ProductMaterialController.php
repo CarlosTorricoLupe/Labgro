f58b548cdcbd23cf596c757a797ddc1d8db55197
Wilder Canaviri Astete <wildersistemas20@gmail.com>
2021-09-10 12:47:49 -0400

    merge

--- app/Http/Controllers/ProductMaterialController.php
+++ app/Http/Controllers/ProductMaterialController.php
@@ -24,7 +24,7 @@ class ProductMaterialController extends Controller
         } else {
             return response()->json([
                 'success'=>false,
-                'message'=>'No se encontraron resultados'
+                'message'=>'No se encontro resultados'
             ],404);
         }
     }
