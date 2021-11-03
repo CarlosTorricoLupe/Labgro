f3d7b6e88c9c9d36775ca23efb8f3d64bae555ad
CarlosTL67 <carlostorricolupe68@gmail.com>
2021-11-03 19:27:19 -0400

    Fix pagination in incomes and outputs

--- app/Models/Income.php
+++ app/Models/Income.php
@@ -37,7 +37,7 @@ class Income extends Model
         }
         return self::select('incomes.id',
         'incomes.receipt','incomes.order_number','provider',
-        'total','invoice_number','created_at')->where('receipt','like',"%$value%")->paginate(2)->appends(request()->query());
+        'total','invoice_number','created_at')->where('receipt','like',"%$value%")->paginate(12)->appends(request()->query());
     }
 
     public static function getIncome($id){
